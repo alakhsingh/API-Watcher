fa5f51f1b0c7a0b8faf64beb05750cd5008fe6b3
Alakh Raghuvanshi <alakh.raghuvanshi@genesys.com>
2019-07-17 13:38:12 -0400

    encrypted password

--- src/main/java/com/api/monitor/API/Monitor/ApiMonitorApplication.java
+++ src/main/java/com/api/monitor/API/Monitor/ApiMonitorApplication.java
@@ -1,10 +1,47 @@
 package com.api.monitor.API.Monitor;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
+@EnableEncryptableProperties
 @SpringBootApplication
 public class ApiMonitorApplication {
+	Logger logger = LoggerFactory.getLogger(ApiMonitorApplication.class);
+
+	@Value("${spring.datasource.driver-class-name}")
+	private String dbDriverClassName;
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@Value("${spring.datasource.username}")
+	private String dbUsername;
+
+	@Value("${EncryptedProperty}")
+	private String myProperty;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(dbDriverClassName);
+		dataSource.setUrl(dbUrl);
+		dataSource.setUsername(dbUsername);
+		dataSource.setPassword(myProperty);
+
+		logger.info("dataSource url: " + dataSource.getUrl());
+		return dataSource;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiMonitorApplication.class, args);
